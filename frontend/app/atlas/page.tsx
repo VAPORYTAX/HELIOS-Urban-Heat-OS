5ea41353912f27e97f532fb24d4a690a4432cdc1
VAPORYTAX <kmoeez986@gmail.com>
2026-08-25 16:54:33 +0500

    Add temporal thermal intelligence to Atlas

--- frontend/app/atlas/page.tsx
+++ frontend/app/atlas/page.tsx
@@ -1,1 +1,3 @@
-import PhoenixAtlas from "@/components/PhoenixAtlas";export default function Page(){return <PhoenixAtlas/>}
+import PhoenixAtlas from "@/components/PhoenixAtlas";
+import ThermalHistoryIntelligence from "@/components/ThermalHistoryIntelligence";
+export default function Page(){return <><PhoenixAtlas/><div className="page atlas-intelligence-page"><ThermalHistoryIntelligence/></div></>}
