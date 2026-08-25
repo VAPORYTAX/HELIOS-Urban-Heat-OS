b71df51a9669b7981ee12f6f949939ea1d67c275
VAPORYTAX <kmoeez986@gmail.com>
2026-08-25 16:47:06 +0500

    Load advanced ThermalWay styles

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import "./decision.css";
 import "./decision-science.css";
 import "./labs.css";
 import "./runtime.css";
+import "./thermalway-intel.css";
 import AppShell from "@/components/AppShell";
 import {DecisionProvider} from "@/components/DecisionContext";
 
